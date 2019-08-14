0ceda3947284aed51256d783191a3c081d27577e
Jason Peng <jason.j.peng@gamil.com>
2019-08-14 14:19:08 -0700

    API  3001 test

--- info/apiDocument.md
+++ info/apiDocument.md
@@ -5,4 +5,10 @@ post: /api/coins/:id/history add (up to date) coin historical price
 get: /api/coins/:id/news   get latest news for the coin 
 get: /api/coins/:id/info   get coin information
 
-  
+post: /api/users     create a new user account
+put:  /api/users/:id  add coins to a user
+
+
+
+
+post: /api/admin/coins  create a collection for all coins with id, name, symbol, logo  
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./apiRoutes.js");
+// const apiRoutes = require("./apiRoutes.js");
+const adminRoutes = require("./adminRoutes.js");
 
 // API Routes
-router.use("/api", apiRoutes);
+router.use("/api/admin", adminRoutes);
+
+
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.status(200).end();
+//  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
