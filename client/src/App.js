4c71df965bea51b7a904094bea54700d8836fac7
Jason Peng <jason.j.peng@gamil.com>
2019-08-15 15:34:14 -0700

    with login

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, {Component} from 'react';
+import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {Motion, spring} from 'react-motion';
+import NavigationPanel from './components/NavigationPanel';
+import Modal from './components/Modal';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			mounted: false
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ mounted: true });
+	}
+	
+	handleSubmit = (e) => {
+		this.setState({ mounted: false });
+		e.preventDefault();
+	}
+
+	render() {
+		const {mounted} = this.state;
+
+		let child;
+		let test = 12;
+
+		if(mounted) {
+			child = (
+				<div className="App_test">
+					<NavigationPanel></NavigationPanel>
+					<Modal onSubmit={this.handleSubmit}/>
+				</div>
+			);
+		}
+		
+		return(
+			<div className="App">
+				<ReactCSSTransitionGroup 
+					transitionName="example"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={300}>
+						{child}
+				</ReactCSSTransitionGroup>
+			</div>
+		);
+	}
 }
 
 export default App;
